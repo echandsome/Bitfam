260597c490746945cb27834bdf2c6baf17157383
echandsome <echandsome@hotmail.com>
2024-11-03 18:09:39 -0800

    Signed-off-by: echandsome <echandsome@hotmail.com>

--- backend/routes/channels.php
+++ backend/routes/channels.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Broadcast;
+    use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('users.{userId}', function ($user, $userId) {
-    return $user->id == $userId;
-});
+    Broadcast::channel('users.{userId}', function ($user, $userId) {
+        return $user->id == $userId;
+    });
